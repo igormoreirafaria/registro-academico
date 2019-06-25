c7427de152e000f0e51363353bc1d57cb1e8bc08
Igor Moreira <igormoreirafaria@gmail.com>
2019-06-24 22:19:33 -0300

    excecoes cliente

--- ControladoraCliente.lua
+++ ControladoraCliente.lua
@@ -11,20 +11,59 @@ function ControladoraCliente:new()
 end
 
 function ControladoraCliente:cadastrar(menu)
-    novoCliente = Cliente:new(menu:inputCadastroEdicao())   
-    if Database:getCliente(novoCliente:getRg()) == nil then
-        Database:addCliente(novoCliente)
+
+    local status, res1, res2, res3, res4 = pcall( 
+        function() 
+            res1, res2, res3, res4 = menu:inputCadastroEdicao() 
+            return res1, res2, res3, res4 
+        end
+    )
+    if status then
+        novoCliente = Cliente:new(res1, res2, res3, res4)
+        if Database:getCliente(novoCliente:getRg()) == nil then
+            Database:addCliente(novoCliente)
+        else
+            print("\nUm ciente com o mesmo rg já existe. Tente inserir este ciente com um rg diferente!") 
+        end
     else
-        print("\nUm ciente com o mesmo rg já existe. Tente inserir este ciente com um rg diferente!")
+        print("ERROR: " .. res1)
     end
 end
 
 function ControladoraCliente:editar(menu)
-    Database:editCliente(menu:inputCadastroEdicao())
+    local status, res1, res2, res3, res4 = pcall( 
+        function() 
+            res1, res2, res3, res4 = menu:inputCadastroEdicao() 
+            return res1, res2, res3, res4 
+        end
+    )
+    if status then
+        if Database:getCliente(res3) == nil then
+            print("\nEste Rg não é válido ou o cliente não foi cadastrado. Digite um Rg válido!")
+        else
+            Database:editCliente(res1,res2,res3,res4)
+        end
+    else
+        print("ERROR: " .. res1)
+    end
 end
 
 function ControladoraCliente:remover(menu)
-    Database:removeCliente(menu:inputRemover())
+    local status, res1 = pcall( 
+        function() 
+            res1 = menu:inputRemover() 
+            return res1 
+        end
+    )
+    if status then
+        if Database:getCliente(res1) == nil then
+            print("\nEste Rg não é válido ou o cliente não foi cadastrado. Digite um Rg válido!")
+        else
+            Database:removeCliente(res1)
+        end
+    else
+        print("ERROR: " .. res1)
+    end
 end
 
 function ControladoraCliente:listar(menu)
